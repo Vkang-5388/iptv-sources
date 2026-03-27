276cf2221dcaa78684b49c62e761796480b1426c
Vkang-5388 <vayken92@gmail.com>
2026-03-27 16:04:18 +0800

    Update index.ts

--- src/sources/index.ts
+++ src/sources/index.ts
@@ -1,3 +1,4 @@
+// 1. 补齐程序运行必需的导出零件
 export * from './epg_pw';
 export * from './iptv_org';
 export * from './yang_m3u';
@@ -9,8 +10,9 @@ export * from './cymz6_lives';
 export * from './youhun';
 export * from './zbds';
 export * from './hotel_tvn';
-export * from './utils';
+export * from './utils'; // 包含 handle_m3u, filter 等关键工具
 
+// 2. 引入配置所需的零件
 import { filter } from '../utils';
 import {
   epg_pw_sources,
@@ -19,7 +21,7 @@ import {
   hotel_tvn_sources,
 } from '.';
 
-// 这里的 sources 是给程序跑的
+// 3. 定义直播源清单
 export const sources: any[] = [
   {
     name: '港澳台免翻',
