c9ea5b6481ab052df67c0e1b4724d035b6dcb9a0
Vkang-5388 <vayken92@gmail.com>
2026-03-27 15:57:54 +0800

    Update index.ts

--- src/sources/index.ts
+++ src/sources/index.ts
@@ -1,34 +1,16 @@
-export * from './epg_pw';
-export * from './iptv_org';
-export * from './yang_m3u';
-export * from './yuechan_live';
-export * from './fanmingming_live';
-export * from './qwerttvv_bj_iptv';
-export * from './joevess_iptv';
-export * from './cymz6_lives';
-export * from './youhun';
-export * from './zbds';
-export * from './hotel_tvn';
-export * from './utils';
-
 import { filter } from '../utils';
-import {
-  epg_pw_sources,
-  youhun_sources,
-  zbds_sources,
-  hotel_tvn_sources,
-} from '.';
 
-export const sources = [
-  // 这里换成了更适合国内网络、不用VPN的源
+export const sources: any[] = [
   {
     name: '港澳台免翻',
     f_name: 'gangtai_no_vpn',
     url: 'https://raw.githubusercontent.com/Guover/iptv/master/gangtai.m3u',
     filter: filter,
   },
-  ...epg_pw_sources,
-  ...youhun_sources,
-  ...zbds_sources,
-  ...hotel_tvn_sources,
+  {
+    name: '酒店电视',
+    f_name: 'hotel',
+    url: 'https://raw.githubusercontent.com/vbskyun/iptv/master/tv.m3u',
+    filter: filter,
+  }
 ];
