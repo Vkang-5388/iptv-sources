e2a627fee40a50e743552624f5dd0952bb5f6a9c
Vkang-5388 <vayken92@gmail.com>
2026-03-27 15:50:54 +0800

    Update index.ts

--- src/sources/index.ts
+++ src/sources/index.ts
@@ -11,24 +11,24 @@ export * from './zbds';
 export * from './hotel_tvn';
 export * from './utils';
 
-import { filter } from '../utils'; // 确保引入 filter
+import { filter } from '../utils';
 import {
   epg_pw_sources,
-  // fanmingming_live_sources, // 暂时关掉旧的，改用下面的专用版
   youhun_sources,
   zbds_sources,
   hotel_tvn_sources,
+  fanmingming_live_sources,
 } from '.';
 
 export const sources = [
-  // 新增：高成功率港澳台源
+  // 1. 新增的高成功率专用源
   {
     name: '港澳台专用',
     f_name: 'gangtai_v2',
     url: 'https://raw.githubusercontent.com/fanmingming/live/main/tv/m3u/ipv6.m3u',
     filter: filter,
   },
-  // ...fanmingming_live_sources, 
+  // 2. 保留你原本的其他源
   ...epg_pw_sources,
   ...youhun_sources,
   ...zbds_sources,
